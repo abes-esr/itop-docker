51432ef913d65bff5ec1f407c37cd411018bf0f4
charot <charot@abes.fr>
2023-11-16 16:15:11 +0100

    Ajout d'itop-talend et suppression d'itop-adminer

---
+++ docker/talend/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Réglage de /etc/environment pour que les crontab s'exécutent avec les bonnes variables d'env
+echo "$(env)
+LANG=en_US.UTF-8" > /etc/environment
+
+# Initialisation de la crontab
+echo "00 20 * * * '$ITOP_TALEND_JOB' 1>/proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/tasks
+echo "-> Installation de la crontab :"
+cat /etc/cron.d/tasks
+crontab /etc/cron.d/tasks
+
+# Force le démarrage du batch au démarrage du conteneur
+echo "-> Lancement de '$ITOP_TALEND_JOB' au démarrage du conteneur"
+$ITOP_TALEND_JOB
+
+# execute CMD (crond)
+exec "$@"
